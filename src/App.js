db5f28bc998a38f9c708a35be2e7f3e19e34dc19
Ezra Farrar <efarrar@rockhopper.cs.pdx.edu>
2019-07-02 12:22:45 -0700

    Added ReactStrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Container>
+
+        <Row className="Header">
+          <Col>Header</Col>
+        </Row>
+          
+        <Row className="Body">
+          <Col>Body</Col>
+        </Row>
+
+      </Container>
     </div>
   );
 }
