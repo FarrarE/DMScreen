f8917f43c12c25b48ace328f2b28d5dcabfd4cd8
Ezra <efarrar@pdx.edu>
2019-07-09 13:26:56 -0700

    Changed file name of data json.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const characterList = require('./data/characters.json');
+const list = require('./data/list.json');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,7 +12,7 @@ app.get('/api/add', (req, res) => {
 
 app.get('/api/list', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.send({ list: characterList });
+    res.send({ list: list });
   });
 
 
