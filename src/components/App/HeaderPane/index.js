7220a454fe0264dc2a0113fbfc92d444b25a76e0
Ezra <efarrar@pdx.edu>
2019-07-09 15:41:27 -0700

    Added header layout.

---
+++ src/components/App/HeaderPane/index.js
@@ -0,0 +1,52 @@
+import React, { UseState } from 'react';
+import { Container, Row, Col} from 'reactstrap';
+import "./styles.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+} from 'reactstrap';
+
+
+class HeaderPane extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: {}
+    };
+    this.populateList = this.populateList.bind(this);
+  }
+  
+
+  populateList(event) {
+    fetch(`/api/list`)
+      .then(response => response.json())
+      .then(state => this.setState(state));
+  }
+
+  render() {
+    {this.populateList()}
+    
+
+    return (
+      <Container fluid>
+        <Row>
+          <Col>
+              <button type="submit">Prev</button>
+              <button type="submit">Next</button>
+  
+          </Col>
+          <Col>DMScreen</Col>
+          <Col>
+              <button type="submit">Sort</button>
+              <button type="submit">Add</button>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default HeaderPane;
