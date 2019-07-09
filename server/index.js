f3f03352355a28ab6a1a574a75dfcea9a8a18beb
Ezra Farrar <efarrar@kororaa.cs.pdx.edu>
2019-07-09 12:34:58 -0700

    server now sends json file to front end for display purposes.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const characterList = require('./data/characters.json');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/files", express.static('files'));
 
 app.get('/api/add', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({ confirmation: `add path called` }));
 });
+
+app.get('/api/list', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send({ list: characterList });
+  });
+
+
 
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
--- src/components/App/ListPane/index.js
+++ src/components/App/ListPane/index.js
@@ -7,19 +7,13 @@ class ListPane extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      confirmation: '',
-      list: [
-        {name:"Test1"},
-        {name:"Test2"},
-        {name:"Test3"},
-        {name:"Test4"},
-      ]
+      list: ''
     };
     this.populateList = this.populateList.bind(this);
   }
 
   populateList(event) {
-    fetch(`/api/add`)
+    fetch(`/api/list`)
       .then(response => response.json())
       .then(state => this.setState(state));
   }
