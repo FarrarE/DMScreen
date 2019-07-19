ecc0dcc997af5062b2ccf0ccd684820f6feb1a2b
Ezra <efarrar@pdx.edu>
2019-07-18 20:48:25 -0700

    Added css on hover effects.

--- src/components/App/ListPane/index.js
+++ src/components/App/ListPane/index.js
@@ -9,7 +9,7 @@ const ListPane = (props) => {
 
   return (
       <Container className="list-pane">
-        {props.list.map(({name, key, type, init}) => <Player name={name} type={type} key={key} init={init}/>)}
+        {props.list.map(({name, key, type, init}) => <Player className="player" name={name} type={type} key={key} init={init}/>)}
       </Container>
   )
 }
--- src/components/App/Player/index.js
+++ src/components/App/Player/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Container, Col, Row, TabPane, TabContent, Nav, NavItem, NavLink } from 'reactstrap';
 import './styles.css'
 
 const Player = (props) => {
 
   return (
-    <div className="player-card">
-      {JSON.stringify(props.name)}
-      {JSON.stringify(props.key)}
-      {JSON.stringify(props.type)}
-      {JSON.stringify(props.init)}
-    </div>
+    <Container className="player-card">
+      <Row>
+        <Col></Col>
+        <Col>
+          {JSON.stringify(props.name)}
+          {JSON.stringify(props.key)}
+          {JSON.stringify(props.type)}
+          {JSON.stringify(props.init)}
+        </Col>
+        <Col>
+          <div className="close">X</div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
