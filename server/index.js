26bbb7c0a421de1c09b25d4d01799977ade3097a
Ezra <efarrar@pdx.edu>
2019-07-18 19:27:13 -0700

    Updates data.json when post request is made.

--- server/index.js
+++ server/index.js
@@ -7,13 +7,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/api/add', (req, res) => {
-  console.log(req.body.name);
-  res.send();
+
+  var fs = require('fs')
+
+  fs.readFile('./server/data/data.json', 'utf-8', function(err, data) {
+
+
+    var arrayOfObjects = JSON.parse(data)
+    arrayOfObjects.list.push({
+      name: req.body.name,
+      init: req.body.init
+    })
+  
+    fs.writeFile('./server/data/data.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+      if (err) throw err
+      console.log('Done!')
+    })
+  })
 });
 
 app.get('/api/list', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.json({ list: data.list});
+
 });
 
 app.listen(3001, () =>
