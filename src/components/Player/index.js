99637e3064e3a8c114854e31a9eb407f2b6e9cbb
Ezra <ezra.farrar@gmail.com>
2020-11-10 22:35:34 -0800

    Changed init to a input so that it can be updated easier

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -4,46 +4,50 @@ import './styles.css'
 import monster from './assets/Orc.png';
 import player from "./assets/Player.png";
 
-function Player(props){
+function Player(props) {
   const [icon, setIcon] = useState("");
   useEffect(() => {
-    if(props.player){
-      if(props.player.type === "monster")
+    if (props.player) {
+      if (props.player.type === "monster")
         setIcon(<img className="monster-icon" src={monster} alt={"..."}></img>);
-      else  
+      else
         setIcon(<img className="player-icon" src={player} alt={"..."}></img>);
     }
 
-  },[props.player]);
+  }, [props.player]);
 
   function handleRemove(event) {
     props.remove(props.player.ukey);
   }
 
+  function updateInput(event) {
+    event.target.placeholder = event.target.value;
+  }
+
   return (
     <Container className="player-card">
-      {props.player && 
+      {props.player &&
         <Row>
-          <Col xs={{  order: 1, size:2}} md={{ size:2, order: 1 }}>
+          <Col xs={{ order: 1, size: 2 }} md={{ size: 2, order: 1 }}>
             <div className="icon-col" >
               <div className="icon-div">
                 {icon}
               </div>
             </div>
           </Col>
-          <Col xs={{  order: 2, size:3 }} md={{ size:3, order: 1 }}>
+          <Col xs={{ order: 2, size: 3 }} md={{ size: 3, order: 1 }}>
             <label><h5>Name</h5></label><br></br>
             {props.player.name}
           </Col>
-          <Col xs={{  order: 2, size:3 }} md={{ size:3, order: 1 }}>
+          <Col xs={{ order: 2, size: 3 }} md={{ size: 3, order: 1 }}>
             <label><h5>Type</h5></label><br></br>
             {props.player.type}
           </Col>
-          <Col xs={{  order: 2, size:3 }} md={{ size:3, order: 1 }}>
+          <Col xs={{ order: 2, size: 3 }} md={{ size: 3, order: 1 }}>
             <label><h5>Initiative</h5></label><br></br>
-            {props.player.init}
+            <input className="init-input" type="number" placeholder={props.player.init} maxLength="3" onChange={updateInput} />
           </Col>
-          <Col className="close-col" xs={{  order: 3 }} md={{ size:1, order: 1 }}>
+          <Col className="close-col" xs={{ order: 3 }} md={{ size: 1, order: 1 }}>
             <button className="button" onClick={handleRemove} className="close">X</button>
           </Col>
         </Row>
