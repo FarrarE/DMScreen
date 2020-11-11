dba2ca373074fb94cc9dcad08593cd8ab700f9aa
Ezra <ezra.farrar@gmail.com>
2020-11-10 22:20:49 -0800

    Added examples programmed by default so new users can see how it works.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row} from 'reactstrap';
+import { Container, Row } from 'reactstrap';
 import './App.css';
 
 import HeaderPane from "./components/HeaderPane";
@@ -7,34 +7,39 @@ import CurrentPane from "./components/CurrentPane";
 import ListPane from "./components/ListPane";
 import AddPane from './components/AddPane';
 
-export default function App(){
+export default function App() {
   const [list, setList] = useState();
   const [current, setCurrent] = useState(0);
   const [addPaneOpen, setAddPaneOpen] = useState(false);
 
   useEffect(() => {
 
-    if(!list){
-      populateList();
+    let storedList = JSON.parse(localStorage.getItem("playerList"));
+    if (storedList) {
+      setList(storedList);
+    }
+    else {
+      setList([{ name: "Example Player", ukey: "example1", type: "player", init: 10 },
+      { name: "Example Monster", ukey: "example2", type: "monster", init: 5 }])
     }
 
-  }, [list]);
+  }, []);
 
   // Gets a list from the server api route and saves it to props list
   function populateList(event) {
     setList(JSON.parse(localStorage.getItem("playerList")));
   }
 
-  function saveList(){
+  function saveList() {
     localStorage.setItem('playerList', JSON.stringify(list));
   }
 
   // Sorts the props list in descending order based on the init property
-  function sortList(){
+  function sortList() {
 
     let newList;
 
-    if(!list)
+    if (!list)
       newList = [];
     else
       newList = [...list];
@@ -44,32 +49,32 @@ export default function App(){
   }
 
   // Decrements current -1, changing what is displayed in the currentPane
-  function previousPlayer(){
-    if(current === 0){
+  function previousPlayer() {
+    if (current === 0) {
       setCurrent(list.length - 1);
-    }else
+    } else
       setCurrent(current - 1);
   }
 
   // Increments tcurrent +1, changing what is displayed in the currentPane
-  function nextPlayer(){
+  function nextPlayer() {
 
-    if(current === list.length - 1){
+    if (current === list.length - 1) {
       setCurrent(0);
-    }else
+    } else
       setCurrent(current + 1);
   }
 
   // Shows or hides AddPane 
-  function toggleAddPane(){
+  function toggleAddPane() {
     setAddPaneOpen(!addPaneOpen);
   }
 
   // Adds an object to props list
-  function addNewPlayer(dataToAdd){
+  function addNewPlayer(dataToAdd) {
     let newList;
 
-    if(!list)
+    if (!list)
       newList = [dataToAdd];
     else
       newList = [...list, dataToAdd];
@@ -79,16 +84,16 @@ export default function App(){
 
   // This function will be called onClick
   // It will remove an item from the props list with a key value equal to dataToRemove
-  function removeButton(dataToRemove){
+  function removeButton(dataToRemove) {
     let newList = [...list];
     let index = -1;
-    for (var i=0; i < newList.length; i++) {
+    for (var i = 0; i < newList.length; i++) {
       if (newList[i].ukey === dataToRemove) {
-          index = i;
+        index = i;
       }
-    } 
-    
-    if(index !== -1){
+    }
+
+    if (index !== -1) {
       newList.splice(index, 1);
       setList(newList);
     }
@@ -97,30 +102,30 @@ export default function App(){
 
   return (
 
-    <Container className="app"> 
-    {addPaneOpen && <AddPane add={addNewPlayer} toggle={toggleAddPane} />}
-    <Row className="Header">
-      <HeaderPane 
-        load={populateList}
-        save={saveList}
-        add={toggleAddPane} 
-        sort={sortList} 
-      />
-    </Row>
-    <Row className="Current">
-      <CurrentPane 
-        previous={previousPlayer} 
-        next={nextPlayer} 
-        remove={removeButton} 
-        currentPlayer={list && list[current]}
-      />
-    </Row>
-    <Row className="List">
-      <ListPane 
-        remove={removeButton} 
-        players={list && list}
-      />
-    </Row>
+    <Container className="app">
+      {addPaneOpen && <AddPane add={addNewPlayer} toggle={toggleAddPane} />}
+      <Row className="Header">
+        <HeaderPane
+          load={populateList}
+          save={saveList}
+          add={toggleAddPane}
+          sort={sortList}
+        />
+      </Row>
+      <Row className="Current">
+        <CurrentPane
+          previous={previousPlayer}
+          next={nextPlayer}
+          remove={removeButton}
+          currentPlayer={list && list[current]}
+        />
+      </Row>
+      <Row className="List">
+        <ListPane
+          remove={removeButton}
+          players={list && list}
+        />
+      </Row>
     </Container>
   )
 }
